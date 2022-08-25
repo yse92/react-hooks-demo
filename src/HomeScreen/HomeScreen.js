50a7716594c90057f5a12fe4a3b0bd3b21132bde
yse92 <yarik49best@gmail.com>
2022-08-25 15:42:42 +0300

    unnecessary comments removed

--- src/HomeScreen/HomeScreen.js
+++ src/HomeScreen/HomeScreen.js
@@ -4,31 +4,6 @@ import {homeStyles} from './HomeScreen.styles'
 import ButtonGroup from './ButtonGroup';
 import {screens} from '../screens'
 
-// export default class HomeScreen extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       selectedIndex: 1,
-//       screens: ['Lifecycle example', 'useEffect example', 'useMemo example', 'useRef example']
-//     }
-//     this.moveToScreen = selectedIndex => {
-//       const {screens} = this.state;
-//       this.props.navigation.navigate(screens[selectedIndex])
-//     }
-//   }
-//   render() {
-//     const buttons = ['Lifecycle example', 'useEffect example', 'useMemo example', 'useRef example']
-//     return (
-//       <View style={homeStyles.container}>
-//         <Text>React tutorials</Text>
-//         <ButtonGroup
-//           onPress={this.moveToScreen}
-//           buttons={buttons} />
-//       </View>
-//     )
-//   }
-// }
-
 export const HomeScreen = ({navigation}) => {
 
   const moveToScreen = screen => {
